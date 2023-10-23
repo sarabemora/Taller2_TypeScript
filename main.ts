5e8514d19e84bb0f2832a8dbe0e422483311c8d9
Sara Benavides <s.benavidesm@uniandes.edu.co>
2023-10-22 19:38:12 -0500

    avance cards

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { dataSeries } from "./dataSeries.js";
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 const promTemporadas: HTMLElement = document.getElementById("promedio")!;
+let cardBody: HTMLElement = document.getElementById('id')!;
 
 desplegarSeries(dataSeries);
 promTemporadas.innerHTML = `${getPromedioTemporadas(dataSeries)}`;
@@ -12,11 +13,26 @@ function desplegarSeries(series: Serie[]): void {
     series.forEach(c => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td>${c.numero}</td>
-                                <td>${c.nombre}</td>
+                                <td class="show-card">${c.nombre}</td>
                                 <td>${c.plataforma}</td>
                                 <td>${c.temporadas}</td>`;
+
+        trElement.querySelector('.show-card')?.addEventListener('click', () => desplegarCard(c))
         seriesTbody.appendChild(trElement);
     })
+}
+
+function desplegarCard(serie: Serie): void {
+    const cardElement: HTMLElement | null = document.querySelector('#id')!;
+    cardElement.innerHTML = `<div class="card" style="width: 20rem;">
+                            <img class="card-img-top" src="${serie.link_serie}" alt="${serie.nombre}">
+                            <div class="card-body">
+                                <h5 class="card-title">${serie.nombre}</h5>
+                                <p class="card-text">${serie.descripcion}</p>
+                                <a href="${serie.link_plataforma}">${serie.link_plataforma}</a>
+                            </div>
+                            </div>`;
+    cardBody.appendChild(cardElement);
 }
 
 function getPromedioTemporadas(series: Serie[]): number {
